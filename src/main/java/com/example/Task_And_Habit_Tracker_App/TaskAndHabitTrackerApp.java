cccc346984b870418f6cd7007eb57f92bed7c285
Christopher Canenguez <canenguez.chris@gmail.com>
2025-04-26 15:59:49 -0400

    Initial Commit for Task and Habit Tracker App.

---
+++ src/main/java/com/example/Task_And_Habit_Tracker_App/TaskAndHabitTrackerApp.java
@@ -0,0 +1,13 @@
+package com.example.Task_And_Habit_Tracker_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAndHabitTrackerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAndHabitTrackerApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Task_And_Habit_Tracker_App/TaskAndHabitTrackerAppTests.java
@@ -0,0 +1,13 @@
+package com.example.Task_And_Habit_Tracker_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAndHabitTrackerAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
